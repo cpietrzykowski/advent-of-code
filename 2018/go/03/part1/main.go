42eafc1fe01befc62b3abe48b2a983f14176aff8
Christopher Pietrzykowski <cpietrzykowski@gmail.com>
2018-12-08 00:43:17 -0700

    add day 03

---
+++ 2018/go/03/part1/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	inputs "go/common"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	inputspath := filepath.Join(inputs.ModulePath(), "../../inputs/data/03/input.txt")
+	if file, error := os.Open(inputspath); error == nil {
+		defer file.Close()
+		overclaimed := overlappingClaims(file)
+		log.Println(overclaimed)
+	} else {
+		log.Println(error)
+	}
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Size struct {
+	Width  int
+	Height int
+}
+
+type Claim struct {
+	Id       int
+	Location Point
+	Size     Size
+}
+
+type Registry map[int]map[int][]*Claim
+
+func (registry *Registry) Register(claim Claim) {
+	for y := claim.Location.Y; y < (claim.Location.Y + claim.Size.Height); y++ {
+		for x := claim.Location.X; x < (claim.Location.X + claim.Size.Width); x++ {
+			if _, ok := (*registry)[y]; !ok {
+				(*registry)[y] = make(map[int][]*Claim)
+			}
+
+			if _, ok := (*registry)[y][x]; !ok {
+				(*registry)[y][x] = []*Claim{}
+			}
+
+			(*registry)[y][x] = append((*registry)[y][x], &claim)
+		}
+	}
+}
+
+func overlappingClaims(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+	registry := Registry{}
+	for scanner.Scan() {
+		if claim, error := makeClaim(scanner.Text()); error == nil {
+			registry.Register(*claim)
+		} else {
+			log.Println(error)
+		}
+	}
+
+	overclaimed := 0
+	for _, row := range registry {
+		for _, col := range row {
+			if len(col) > 1 {
+				overclaimed++
+			}
+		}
+	}
+
+	return overclaimed
+}
+
+func makeClaim(entry string) (*Claim, error) {
+	// #12 @ 269,129: 25x14
+	re := regexp.MustCompile(`#(\d+)\s*@\s*(\d+),(\d+):\s+(\d+)x(\d+)`)
+	fields := re.FindAllStringSubmatch(entry, -1)
+
+	if (fields != nil) && (len(fields[0]) == 6) {
+		if id, error := strconv.Atoi(fields[0][1]); error == nil {
+			var point Point
+			if xpos, error := strconv.Atoi(fields[0][2]); error == nil {
+				if ypos, error := strconv.Atoi(fields[0][3]); error == nil {
+					point = Point{xpos, ypos}
+				} else {
+					return nil, error
+				}
+			} else {
+				return nil, error
+			}
+
+			var size Size
+			if width, error := strconv.Atoi(fields[0][4]); error == nil {
+				if height, error := strconv.Atoi(fields[0][5]); error == nil {
+					size = Size{width, height}
+				} else {
+					return nil, error
+				}
+			} else {
+				return nil, error
+			}
+
+			return &Claim{id, point, size}, nil
+		} else {
+			return nil, error
+		}
+	}
+
+	return nil, nil
+}
---
+++ 2018/go/03/part2/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	inputs "go/common"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	inputspath := filepath.Join(inputs.ModulePath(), "../../inputs/data/03/input.txt")
+	if file, error := os.Open(inputspath); error == nil {
+		defer file.Close()
+		validclaims := validClaims(file)
+		log.Println(validclaims)
+	} else {
+		log.Println(error)
+	}
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+func (point Point) String() string {
+	return fmt.Sprintf("%d,%d", point.X, point.Y)
+}
+
+type Size struct {
+	Width  int
+	Height int
+}
+
+func (size Size) String() string {
+	return fmt.Sprintf("%dx%d", size.Width, size.Height)
+}
+
+type Claim struct {
+	Id       int
+	Location Point
+	Size     Size
+}
+
+func (claim Claim) String() string {
+	return fmt.Sprintf("%06d: (%s) %s", claim.Id, claim.Location, claim.Size)
+}
+
+type Registry struct {
+	Claims []*Claim
+	Mapped map[int]map[int][]*Claim
+}
+
+func NewRegistry() *Registry {
+	r := new(Registry)
+	r.Claims = []*Claim{}
+	r.Mapped = map[int]map[int][]*Claim{}
+	return r
+}
+
+func (registry *Registry) Register(claim *Claim) {
+	registry.Claims = append(registry.Claims, claim)
+	for y := claim.Location.Y; y < (claim.Location.Y + claim.Size.Height); y++ {
+		for x := claim.Location.X; x < (claim.Location.X + claim.Size.Width); x++ {
+			if _, ok := registry.Mapped[y]; !ok {
+				registry.Mapped[y] = make(map[int][]*Claim)
+			}
+
+			if _, ok := registry.Mapped[y][x]; !ok {
+				registry.Mapped[y][x] = []*Claim{}
+			}
+
+			registry.Mapped[y][x] = append(registry.Mapped[y][x], claim)
+		}
+	}
+}
+
+func (registry *Registry) ValidateClaim(claim *Claim) bool {
+	for y := claim.Location.Y; y < (claim.Location.Y + claim.Size.Height); y++ {
+		for x := claim.Location.X; x < (claim.Location.X + claim.Size.Width); x++ {
+			if len(registry.Mapped[y][x]) > 1 {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func validClaims(file *os.File) []Claim {
+	scanner := bufio.NewScanner(file)
+	registry := NewRegistry()
+	for scanner.Scan() {
+		if claim, error := makeClaim(scanner.Text()); error == nil {
+			registry.Register(claim)
+		} else {
+			log.Println(error)
+		}
+	}
+
+	validclaims := []Claim{}
+	for _, claim := range registry.Claims {
+		if registry.ValidateClaim(claim) {
+			validclaims = append(validclaims, *claim)
+		}
+	}
+
+	return validclaims
+}
+
+func makeClaim(entry string) (*Claim, error) {
+	// #12 @ 269,129: 25x14
+	re := regexp.MustCompile(`#(\d+)\s*@\s*(\d+),(\d+):\s+(\d+)x(\d+)`)
+	fields := re.FindAllStringSubmatch(entry, -1)
+
+	if (fields != nil) && (len(fields[0]) == 6) {
+		if id, error := strconv.Atoi(fields[0][1]); error == nil {
+			var point Point
+			if xpos, error := strconv.Atoi(fields[0][2]); error == nil {
+				if ypos, error := strconv.Atoi(fields[0][3]); error == nil {
+					point = Point{xpos, ypos}
+				} else {
+					return nil, error
+				}
+			} else {
+				return nil, error
+			}
+
+			var size Size
+			if width, error := strconv.Atoi(fields[0][4]); error == nil {
+				if height, error := strconv.Atoi(fields[0][5]); error == nil {
+					size = Size{width, height}
+				} else {
+					return nil, error
+				}
+			} else {
+				return nil, error
+			}
+
+			return &Claim{id, point, size}, nil
+		} else {
+			return nil, error
+		}
+	}
+
+	return nil, nil
+}
